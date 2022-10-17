82ac5359c7501f3f5a798a6d687e53e9f5f2e414
Julia Swan <julie112397@gmail.com>
2022-10-17 16:23:11 +0500

    Task8 completed

---
+++ HW_1/Zadacha8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1) Console.WriteLine("Число должно быть положительным.");
+if (number == 1) Console.WriteLine("Чётных чисел нет.");
+
+Console.Write("Чётные числа: ");
+int i = 2;
+while (i < number) 
+{
+    Console.Write($"{i}, ");
+    i += 2;
+}
+if (i == number) Console.Write($"{i}.");
