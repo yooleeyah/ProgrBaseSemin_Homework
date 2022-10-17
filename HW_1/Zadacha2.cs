489ec58429e13dad4798bc8e917592e57570ddd9
Julia Swan <julie112397@gmail.com>
2022-10-17 15:26:07 +0500

    Task2 completed

---
+++ HW_1/Zadacha2.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2) Console.WriteLine($"max = {number1}");
+if (number1 <= number2) Console.WriteLine($"max = {number2}");
