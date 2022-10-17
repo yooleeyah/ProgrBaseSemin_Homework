89ad9a31b9c137d4ce83f93809bcb237f8d7d14b
Julia Swan <julie112397@gmail.com>
2022-10-17 15:36:19 +0500

    Task4 completed

---
+++ HW_1/Zadacha4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.WriteLine($"max = {max}");
