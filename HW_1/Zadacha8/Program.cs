0d09b247d9935d2ce68660238fc15af982423aca
Julia Swan <julie112397@gmail.com>
2022-10-17 16:37:50 +0500

    Task8 completed

--- HW_1/Zadacha8/Program.cs
+++ HW_1/Zadacha8/Program.cs
@@ -3,11 +3,14 @@ int number = Convert.ToInt32(Console.ReadLine());
 if (number < 1) Console.WriteLine("Число должно быть положительным.");
 if (number == 1) Console.WriteLine("Чётных чисел нет.");
 
-Console.Write("Чётные числа: ");
-int i = 2;
-while (i < number) 
+if (number > 1) 
 {
-    Console.Write($"{i}, ");
-    i += 2;
+    Console.Write("Чётные числа: ");
+    int i = 2;
+    while (i < number) 
+    {
+        Console.Write($"{i}, ");
+        i += 2;
+    }
+    if (i == number) Console.Write($"{i}.");
 }
-if (i == number) Console.Write($"{i}.");
