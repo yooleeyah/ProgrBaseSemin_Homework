9325e7ca65618d6012bd609b456119d16f4e7d43
Julia Swan <julie112397@gmail.com>
2022-10-17 15:41:55 +0500

    Task6 completed

---
+++ HW_1/Zadacha6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine("Число чётное.");
+else Console.WriteLine("Число нечётное.");
