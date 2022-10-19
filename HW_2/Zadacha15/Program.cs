ab0fc46a1a962489af5b9be10eeff6b7f812d7fd
Julia Swan <julie112397@gmail.com>
2022-10-20 03:19:46 +0500

    Task15 completed

---
+++ HW_2/Zadacha15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1 || number > 7) Console.WriteLine("Такого дня недели не существует.");
+else
+{
+    if (number == 6 || number == 7) Console.WriteLine("Выходной");
+    else Console.WriteLine("Не выходной");
+}
