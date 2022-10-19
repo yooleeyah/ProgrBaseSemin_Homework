fce6d7687f086db7e0833822882209514192c602
Julia Swan <julie112397@gmail.com>
2022-10-20 03:09:45 +0500

    Task13 completed

---
+++ HW_2/Zadacha13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) Console.WriteLine("Третьей цифры нет");
+if (number > 99 && number < 1000) Console.WriteLine(number % 10);
+if (number > 999) 
+{
+    while (number > 1000) number = number / 10;
+    Console.WriteLine(number % 10);
+}
