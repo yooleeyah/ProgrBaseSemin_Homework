0670f60c7af5a0d105f3a517b46fe472d0b9932f
Julia Swan <julie112397@gmail.com>
2022-10-21 20:31:02 +0500

    task23 completed

---
+++ HW_3/Zadacha23/Program.cs
@@ -0,0 +1,8 @@
+﻿void PrintSquareChart(int number)
+{
+    for (int i = 1; i <= number; i++) Console.Write($"{i*i*i}, ");
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintSquareChart(number);
