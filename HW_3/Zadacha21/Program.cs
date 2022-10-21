0823a1553fa7fc82242c2edcb9b4ff3bf7854913
Julia Swan <julie112397@gmail.com>
2022-10-21 20:25:26 +0500

    task21 completed

---
+++ HW_3/Zadacha21/Program.cs
@@ -0,0 +1,16 @@
+﻿double GetDistance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+}
+
+Console.Write("Введите координаты первой точки через Enter: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты второй точки через Enter: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = GetDistance(x1, y1, z1, x2, y2, z2);
+Console.WriteLine(distance);
