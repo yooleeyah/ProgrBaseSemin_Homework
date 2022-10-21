3d3f3abd1d7b0b901214ebeb0048262d5f099479
Julia Swan <julie112397@gmail.com>
2022-10-21 20:19:11 +0500

    task19 completed

---
+++ HW_3/Zadacha19/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintIfPolyndrom(int number)
+{
+    if ((number/10000) == (number % 10))
+    {
+        number = number/10;
+        if (((number/100) % 10) == (number % 10)) Console.WriteLine("Число - палиндром.");
+        else Console.WriteLine("Число - не палиндром.");
+    }
+    else Console.WriteLine("Число - не палиндром.");
+}
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintIfPolyndrom(number);
