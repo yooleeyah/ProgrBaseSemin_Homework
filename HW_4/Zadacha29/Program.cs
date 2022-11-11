16a04a8b34cfac92205fd49842751609bd42cb9f
Julia Swan <julie112397@gmail.com>
2022-11-12 00:39:53 +0500

    task29 completed

---
+++ HW_4/Zadacha29/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] array = new int[8];
+Console.WriteLine("Введите 8 элементов массива через Enter");
+for (int i = 0; i < 8; i++) array[i] = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < 8; i++) Console.Write($"{array[i]}, ");
