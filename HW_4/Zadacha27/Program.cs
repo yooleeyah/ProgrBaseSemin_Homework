ee00f1c1a54da3dda91111f32564ecd80178df08
Julia Swan <julie112397@gmail.com>
2022-11-12 00:29:42 +0500

    task25 completed

---
+++ HW_4/Zadacha27/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int count = 1;
+for (int number = Convert.ToInt32(Console.ReadLine()); (number/10) >= 1; number /= 10) count++;
+Console.WriteLine($"Цифр в числе: {count}");
