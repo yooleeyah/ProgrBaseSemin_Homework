da50d11015d1e1a7fde98192ff337075503e9b5f
Julia Swan <julie112397@gmail.com>
2022-11-12 00:03:12 +0500

    task27 completed

---
+++ HW_4/Zadacha25/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число, которое нужно возвести в степень: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите в какую степень нужно возвести: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int exponent = 1;
+for (int i = 1; i <= numberB; i++) exponent *= numberA;
+Console.WriteLine(exponent);
