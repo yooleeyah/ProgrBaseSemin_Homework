ed0da9bfe6b4426ecfdc2fa10cedd791233d1bc0
Julia Swan <julie112397@gmail.com>
2022-11-12 21:48:12 +0500

    task34 completed

---
+++ HW_5/Zadacha34/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = new int[4];
+int count = 0;
+for (int i = 0; i < 4; i++) 
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write($"{array[i]}, ");
+    if (array[i] % 2 == 0) count++;
+}
+Console.WriteLine();
+Console.WriteLine(count);
