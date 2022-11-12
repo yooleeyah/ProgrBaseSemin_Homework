ed025ae2f3d49ae741b37cbc2551d87e683ef54a
Julia Swan <julie112397@gmail.com>
2022-11-12 22:28:35 +0500

    task38 completed

---
+++ HW_5/Zadacha38/Program.cs
@@ -0,0 +1,14 @@
+﻿double[] array = new double[4];
+double maxNumb = 0;
+double minNumb = 1;
+for (int i = 0; i < 4; i++) 
+{
+    array[i] = new Random().NextDouble();
+    Console.Write($"{array[i]}, ");
+    if (array[i] > maxNumb) maxNumb = array[i];
+    if (array[i] < minNumb) minNumb = array[i];
+}
+ Console.WriteLine();
+ Console.WriteLine($"max = {maxNumb}");
+ Console.WriteLine($"min = {minNumb}");
+ Console.WriteLine($"Разница = {maxNumb - minNumb}");
