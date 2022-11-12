00e88dd82fdbc77048d671f21efe68de056c996d
Julia Swan <julie112397@gmail.com>
2022-11-12 21:53:06 +0500

    task36 completed

---
+++ HW_5/Zadacha36/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = new int[4];
+int sum = 0;
+for (int i = 0; i < 4; i++) 
+{
+    array[i] = new Random().Next(-10,100);
+    Console.Write($"{array[i]}, ");
+    if (i % 2 != 0) sum += array[i];
+}
+Console.WriteLine();
+Console.WriteLine(sum);
