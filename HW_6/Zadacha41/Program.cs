1e5fb47d5b4e6d01d769be772d71fce340b4129b
Julia Swan <julie112397@gmail.com>
2022-11-16 22:45:06 +0500

    Task41 and task43 completed

---
+++ HW_6/Zadacha41/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите кол-во чисел: ");
+int amount = Convert.ToInt32(Console.ReadLine());
+int posAmount = 0;
+Console.WriteLine("Введите числа через Enter:");
+for(int i = 0; i < amount; i++) 
+{
+    if(Convert.ToInt32(Console.ReadLine()) > 0) posAmount++;
+}
+Console.WriteLine(posAmount);
---
+++ HW_6/Zadacha43/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите k1, b1, k2, b2 через Enter:");
+int k1 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+int k2 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+if (k1 == k2) Console.WriteLine($"Прямые параллельны, точки пересечения нет.");
+else
+{
+    int x = (b2 - b1)/(k1 - k2);
+    int y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения - ({x},{y})");
+}
