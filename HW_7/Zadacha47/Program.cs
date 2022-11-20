f0860fe1109a13e6724bd94c64f7befb06d32c7d
Julia Swan <julie112397@gmail.com>
2022-11-21 00:41:55 +0500

    Task47 done

---
+++ HW_7/Zadacha47/Program.cs
@@ -0,0 +1,23 @@
+﻿void FillArray (double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) array[i,j] = new Random().NextDouble() * new Random().Next(-100,100);
+    }
+}
+
+void PrintArray (double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) Console.Write($"{Math.Round(array[i,j], 2)}   ");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число строк и столбцов через Enter:");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double [m,n];
+FillArray(array);
+PrintArray(array);
