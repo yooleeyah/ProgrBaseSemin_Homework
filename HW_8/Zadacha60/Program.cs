867271f0d57e4740454faf317bdcb6db6dd24355
Julia Swan <julie112397@gmail.com>
2022-11-27 19:00:57 +0500

    Task60 done

---
+++ HW_8/Zadacha60/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray(int[,,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            for (int k = 0; k < array.GetLength(2); k++) 
+            {
+                temp = new Random().Next(10,100);
+                if (Contains(array, temp) == true) k--;
+                else array[i,j,k] = temp;
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            for (int k = 0; k < array.GetLength(2); k++) Console.Write($"{array[i,j,k]}({i},{j},{k}) ");
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
+
+bool Contains(int[,,] arr, int a)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {       
+        for (int j = 0; j < arr.GetLength(1); j++) 
+        {
+            for (int k = 0; k < arr.GetLength(2); k++) if (arr[i,j,k] == a) return true;
+        }
+    }
+    return false;
+}
+
+int[,,] array = new int[2,2,2];
+FillArray(array);
+PrintArray(array);
