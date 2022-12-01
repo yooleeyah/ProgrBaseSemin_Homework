10b6ed93877c7d40425041e033ef3cbe620998d2
Julia Swan <julie112397@gmail.com>
2022-12-01 18:00:47 +0500

    Task66 done

---
+++ HW_9/Zadacha66/Program.cs
@@ -0,0 +1,14 @@
+﻿void FindSum(int m, int n, int sum)
+{
+    if (n == m) Console.Write(sum);
+    else 
+    {
+        sum += n;
+        FindSum(m, n - 1, sum);
+    }
+}
+
+int m = 3;
+int n = 11;
+int sum = m;
+FindSum(m, n, sum);
