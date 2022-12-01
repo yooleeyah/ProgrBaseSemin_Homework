cfcbb24a12a6264c1b53ff7da8951ffc7ceee3d1
Julia Swan <julie112397@gmail.com>
2022-12-01 17:46:10 +0500

    Task64 done

---
+++ HW_9/Zadacha64/Program.cs
@@ -0,0 +1,12 @@
+﻿void PrintNaturals(int n)
+{
+    if (n == 1) Console.Write($"{n} ");
+    else 
+    {
+        Console.Write($"{n} ");
+        PrintNaturals(n - 1);
+    }
+}
+
+int n = 6;
+PrintNaturals(n);
