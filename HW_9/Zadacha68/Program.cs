f6ad9deee54ac54d78f2153fe8e030a425b2ed97
Julia Swan <julie112397@gmail.com>
2022-12-01 18:23:50 +0500

    Task68 done

---
+++ HW_9/Zadacha68/Program.cs
@@ -0,0 +1,12 @@
+﻿int Ackermann (int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if ((m > 0) && (n == 0)) return Ackermann (m - 1, 1);
+        else if ((m > 0) && (n > 0)) return Ackermann (m - 1, Ackermann (m, n - 1));
+            else return -1;
+}
+
+int m = 3; //не вводить больше 3!!!
+int n = 4;
+if (Ackermann(m, n) == -1) Console.WriteLine("Числа заданы неправильно.");
+else Console.WriteLine(Ackermann(m, n));
